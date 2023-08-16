ad51bf2c497a0b0bb835c36f58ff28047fcefc24
diogo <dipm@novonordisk.com>
2023-08-16 15:34:03 +0200

    feat: add export variables script

---
+++ export_env_variables.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+export AWS_REGION='*****'
+export VPC_ID='vpc-*****'
+export PUBLIC_SUBNET1='subnet-*****'
+export PUBLIC_SUBNET2='subnet-*****'
+export ECS_EXEC_BUCKET_NAME='samples-ecs-exec-output-*****'
+export AWS_PROFILE='*****'
