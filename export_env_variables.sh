6f895094b2977a52e8f6b55061ea2bd8f1b7a0d6
diogo <dipm@novonordisk.com>
2023-08-16 16:14:11 +0200

    feat: exported variables

--- export_env_variables.sh
+++ export_env_variables.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
-export AWS_REGION='*****'
-export VPC_ID='vpc-*****'
-export PUBLIC_SUBNET1='subnet-*****'
-export PUBLIC_SUBNET2='subnet-*****'
-export ECS_EXEC_BUCKET_NAME='samples-ecs-exec-output-*****'
-export AWS_PROFILE='*****'
+# source export_env_variables.sh
+export AWS_REGION="*****"
+export VPC_ID="vpc-*****"
+export PUBLIC_SUBNET1="subnet-*****"
+export PUBLIC_SUBNET2="subnet-*****"
+export PROJECT="samples-ecs-exec"
+export ECS_EXEC_BUCKET_NAME="$PROJECT-output-*****"
+export AWS_PROFILE="*****"
